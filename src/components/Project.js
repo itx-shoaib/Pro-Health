972c93112d48a9d1a0ab167dbef7fa353b3f6619
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-07-26 17:49:37 +0500

    Fixing bugs in frontend

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,144 +1,119 @@
 import React from 'react'
 import '../css/Project.css';
+import EmblaCarousel from './EmblaCarousel';
 import Footer from './Footer'
 import Navbar from './Navbar'
 
 
+
+
+
 const Project = () => {
+    const SLIDE_COUNT = 3;
+    const slides = Array.from(Array(SLIDE_COUNT).keys());
   return (
-    <div>
+    <>
         <Navbar/>
-    <div class="scrolloff">
-        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
-                    aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                    aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-                    aria-label="Slide 3"></button>
+    <div className="scrolloff">
+        <div className='divsize'>
+            <EmblaCarousel slides={slides} />
+        </div>
+        
+        <div className="labeldivstyle">
+            <h1 className="labelstyle">Our mission is develop the best design</h1>
+        </div>
+        <div className="row mt-5">
+            <div className="col-md-3">
+                <h5 className="ServiceTitle">Client</h5>
+                <h5 className="ServiceValue">Apple Inc.</h5>
             </div>
-            <div class="carousel-inner">
-                <div class="carousel-item carouselitems active">
-                    <img src="https://img.freepik.com/premium-photo/black-stone-texture-dark-slate-background-top-view_88281-1206.jpg?w=2000"
-                        class="d-block w-100 imgresponsive" alt="..." />
-                    <div class="carousel-caption d-md-block ImgBodyAlignment">
-                        <h1 class="carouselsubtitle">Image 1</h1>
-                    </div>
-                </div>
-                <div class="carousel-item carouselitems">
-                    <img src="https://img.freepik.com/premium-photo/black-stone-texture-dark-slate-background-top-view_88281-1206.jpg?w=2000"
-                        class="d-block w-100 imgresponsive" alt="..." />
-                    <div class="carousel-caption d-md-block ImgBodyAlignment">
-                        <h1 class="carouselsubtitle">Image 2</h1>
-
-                    </div>
-                </div>
-                <div class="carousel-item carouselitems">
-                    <img src="https://img.freepik.com/premium-photo/black-stone-texture-dark-slate-background-top-view_88281-1206.jpg?w=2000"
-                        class="d-block w-100 imgresponsive" alt="..." />
-                    <div class="carousel-caption d-md-block ImgBodyAlignment">
-                        <h1 class="carouselsubtitle">Image 3</h1>
-
-                    </div>
-                </div>
+            <div className="col-md-3">
+                <h5 className="ServiceTitle">Services</h5>
+                <h5 className="ServiceValue">Branding, Design</h5>
+            </div>
+            <div className="col-md-3">
+                <h5 className="ServiceTitle">Date</h5>
+                <h5 className="ServiceValue">Jul 25, 2022</h5>
+            </div>
+            <div className="col-md-3">
+                <h5 className="ServiceTitle">Team</h5>
+                <h5 className="ServiceValue">Paul - Designer</h5>
             </div>
         </div>
-        <div class="labeldivstyle">
-            <h1 class="labelstyle">Our mission is develop the best design</h1>
-        </div>
-        <div class="row mt-5">
-            <div class="col-md-3">
-                <h5 class="ServiceTitle">Client</h5>
-                <h5 class="ServiceValue">Apple Inc.</h5>
-            </div>
-            <div class="col-md-3">
-                <h5 class="ServiceTitle">Services</h5>
-                <h5 class="ServiceValue">Branding, Design</h5>
-            </div>
-            <div class="col-md-3">
-                <h5 class="ServiceTitle">Date</h5>
-                <h5 class="ServiceValue">Jul 25, 2022</h5>
-            </div>
-            <div class="col-md-3">
-                <h5 class="ServiceTitle">Team</h5>
-                <h5 class="ServiceValue">Paul - Designer</h5>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-6 ownersection align-items-center mediaqueryFour">
+        <div className="row">
+            <div className="col-md-6 ownersection align-items-center mediaqueryFour">
                 <img src="https://sparch.droitlab.com/dark/wp-content/uploads/sites/2/2021/03/specialization-1.jpg" />
             </div>
-            <div class="col-md-6 responsivepadding">
-                <h1 class="HeadOne text-center">Specialization</h1>
-                <h5 class="SpecHeadOne text-center">Hospital Management and System</h5>
-                <div class="row rowmargin">
-                    <div class="col-md-6">
+            <div className="col-md-6 responsivepadding">
+                <h1 className="HeadOne text-center">Specialization</h1>
+                <h5 className="SpecHeadOne text-center">Hospital Management and System</h5>
+                <div className="row rowmargin">
+                    <div className="col-md-6">
 
-                        <h5 class="SpecList"> Project Feasibility & Concept development</h5>
-                        <h5 class="SpecList"> Healthcare Facility Design & Planning</h5>
-                        <h5 class="SpecList"> Establish Biomedical Engineering Departments in Hospitals</h5>
-                        <h5 class="SpecList"> Procurement of Electro-medical Equipment.</h5>
+                        <h5 className="SpecList"> Project Feasibility & Concept development</h5>
+                        <h5 className="SpecList"> Healthcare Facility Design & Planning</h5>
+                        <h5 className="SpecList"> Establish Biomedical Engineering Departments in Hospitals</h5>
+                        <h5 className="SpecList"> Procurement of Electro-medical Equipment.</h5>
                     </div>
-                    <div class="col-md-6">
-                        <h5 class="SpecList"> Maintenance Management</h5>
-                        <h5 class="SpecList"> Turnkey Supplies & Execution.</h5>
-                        <h5 class="SpecList"> Turnkey Supplies & Execution.</h5>
-                        <h5 class="SpecList">Healthcare Staff Training</h5>
-                        <h5 class="SpecList">Hospital Project Management</h5>
-                        <h5 class="SpecList"> Turnkey Project</h5>
+                    <div className="col-md-6">
+                        <h5 className="SpecList"> Maintenance Management</h5>
+                        <h5 className="SpecList"> Turnkey Supplies & Execution.</h5>
+                        <h5 className="SpecList"> Turnkey Supplies & Execution.</h5>
+                        <h5 className="SpecList">Healthcare Staff Training</h5>
+                        <h5 className="SpecList">Hospital Project Management</h5>
+                        <h5 className="SpecList"> Turnkey Project</h5>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-6 ownersection responsivepadding">
-                <h1 class="HeadOne">Who We Are?</h1>
-                <h5 class="HeadTwo">
+        <div className="row">
+            <div className="col-md-6 ownersection responsivepadding">
+                <h1 className="HeadOne">Who We Are?</h1>
+                <h5 className="HeadTwo">
                     Pro-Health is Pakistan’s Leading Healthcare Facility Planner, established since 2001. They
                     specialize in the field of Healthcare Engineering ( Design, Planning , Equipment Management),
                     Healthcare Project Management and provide turnkey solutions to it’s client.
 
                 </h5>
-                <h5 class="HeadThree">Dr. Shehzad Nazir Leghari</h5>
-                <h6 class="HeadFour">CEO & Founder</h6>
+                <h5 className="HeadThree">Dr. Shehzad Nazir Leghari</h5>
+                <h6 className="HeadFour">CEO & Founder</h6>
                 <img src="https://sparch.droitlab.com/dark/wp-content/uploads/sites/2/2021/03/signature-1.png" />
             </div>
-            <div class="col-md-6 ownersection">
+            <div className="col-md-6 ownersection">
                 <img src="https://sparch.droitlab.com/dark/wp-content/uploads/sites/2/2021/03/who-we-1.jpg" />
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-6 ownersection align-items-center mediaqueryFour">
+        <div className="row">
+            <div className="col-md-6 ownersection align-items-center mediaqueryFour">
                 <img src="https://sparch.droitlab.com/dark/wp-content/uploads/sites/2/2021/03/specialization-1.jpg" />
             </div>
-            <div class="col-md-6 responsivepadding">
-                <h1 class="HeadOne text-center">Specialization</h1>
-                <h5 class="SpecHeadOne text-center">Hospital Management and System</h5>
-                <div class="row rowmargin">
-                    <div class="col-md-6">
+            <div className="col-md-6 responsivepadding">
+                <h1 className="HeadOne text-center">Specialization</h1>
+                <h5 className="SpecHeadOne text-center">Hospital Management and System</h5>
+                <div className="row rowmargin">
+                    <div className="col-md-6">
 
-                        <h5 class="SpecList"> Project Feasibility & Concept development</h5>
-                        <h5 class="SpecList"> Healthcare Facility Design & Planning</h5>
-                        <h5 class="SpecList"> Establish Biomedical Engineering Departments in Hospitals</h5>
-                        <h5 class="SpecList"> Procurement of Electro-medical Equipment.</h5>
+                        <h5 className="SpecList"> Project Feasibility & Concept development</h5>
+                        <h5 className="SpecList"> Healthcare Facility Design & Planning</h5>
+                        <h5 className="SpecList"> Establish Biomedical Engineering Departments in Hospitals</h5>
+                        <h5 className="SpecList"> Procurement of Electro-medical Equipment.</h5>
                     </div>
-                    <div class="col-md-6">
-                        <h5 class="SpecList"> Maintenance Management</h5>
-                        <h5 class="SpecList"> Turnkey Supplies & Execution.</h5>
-                        <h5 class="SpecList"> Turnkey Supplies & Execution.</h5>
-                        <h5 class="SpecList">Healthcare Staff Training</h5>
-                        <h5 class="SpecList">Hospital Project Management</h5>
-                        <h5 class="SpecList"> Turnkey Project</h5>
+                    <div className="col-md-6">
+                        <h5 className="SpecList"> Maintenance Management</h5>
+                        <h5 className="SpecList"> Turnkey Supplies & Execution.</h5>
+                        <h5 className="SpecList"> Turnkey Supplies & Execution.</h5>
+                        <h5 className="SpecList">Healthcare Staff Training</h5>
+                        <h5 className="SpecList">Hospital Project Management</h5>
+                        <h5 className="SpecList"> Turnkey Project</h5>
                     </div>
                 </div>
             </div>
         </div>
-        <hr class="hr" />
-        {/* <div class="row justify-content-center paddingrow"> */}
+        <hr className="hr" />
+        {/* <div className="row justify-content-center paddingrow"> */}
             <Footer/>
     </div>
-    </div>
+    </>
   )
 }
 
