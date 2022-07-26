4c05ff46818bd2fe8d0cf5fb68c44cc11c298682
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-07-26 18:06:16 +0500

    adding AOS animation in home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,20 @@ import React from 'react'
 import '../css/HomePage.css'
 import Footer from './Footer'
 import Navbar from './Navbar'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init({
+    duration:2000
+});
 
 const Home = () => {
     return (
         <div>
             <Navbar/>
             <div className="scrolloff">
-                <div
+                <div 
+                    data-aos='fade-down'
                     id="carouselExampleCaptions"
                     className="carousel slide"
                     data-bs-ride="carousel"
@@ -77,19 +84,19 @@ const Home = () => {
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-md-6 ownersection">
-                        <h1 className="HeadOne">Who We Are?</h1>
-                        <h5 className="HeadTwo">
+                    <div  className="col-md-6 ownersection">
+                        <h1 data-aos='fade-left' className="HeadOne">Who We Are?</h1>
+                        <h5 data-aos='fade-left' className="HeadTwo">
                             Pro-Health is Pakistan’s Leading Healthcare Facility Planner, established since 2001. They specialize in the field of Healthcare Engineering ( Design, Planning , Equipment Management), Healthcare Project Management and provide turnkey solutions to it’s client.
 
                         </h5>
-                        <h5 className="HeadThree">Dr. Shehzad Nazir Leghari</h5>
-                        <h6 className="HeadFour">CEO & Founder</h6>
+                        <h5 data-aos='fade-left' className="HeadThree">Dr. Shehzad Nazir Leghari</h5>
+                        <h6 data-aos='fade-left' className="HeadFour">CEO & Founder</h6>
                         <img
-                            alt='...' src="https://sparch.droitlab.com/dark/wp-content/uploads/sites/2/2021/03/signature-1.png"
+                           data-aos='fade-left' alt='...' src="https://sparch.droitlab.com/dark/wp-content/uploads/sites/2/2021/03/signature-1.png"
                         />
                     </div>
-                    <div className="col-md-6 ownersection">
+                    <div data-aos='fade-right' className="col-md-6 ownersection">
                         <img
                             alt='...' src="https://sparch.droitlab.com/dark/wp-content/uploads/sites/2/2021/03/who-we-1.jpg"
                         />
@@ -98,10 +105,10 @@ const Home = () => {
                 <div className="row">
                     <div className="col-md-6 ownersection align-items-center mediaqueryFour">
                         <img
-                            alt='...' src="https://sparch.droitlab.com/dark/wp-content/uploads/sites/2/2021/03/specialization-1.jpg"
+                           data-aos='fade-right' alt='...' src="https://sparch.droitlab.com/dark/wp-content/uploads/sites/2/2021/03/specialization-1.jpg"
                         />
                     </div>
-                    <div className="col-md-6 mediaqueryspec">
+                    <div className="col-md-6 mediaqueryspec" data-aos='fade-left'>
                         <h1 className="HeadOne text-center">Specialization</h1>
                         <h5 className="SpecHeadOne text-center">Hospital Management and System</h5>
                         <div className="row rowmargin">
@@ -125,10 +132,10 @@ const Home = () => {
                 </div>
                 <div className="row mediaquerythree">
                     <div className="col-md-6">
-                        <h1 className="ProjectHeadTwo text-center">Latest Projects</h1>
+                        <h1 data-aos='fade-right' className="ProjectHeadTwo text-center">Latest Projects</h1>
                     </div>
                     <div className="col-md-6">
-                        <h6 className="ProjectHeadTwo">
+                        <h6 data-aos='fade-left' className="ProjectHeadTwo">
                             Rawalpindi Institute of Cardiology
                         </h6>
                     </div>
@@ -173,7 +180,7 @@ const Home = () => {
                 </div>
             </div>
         </div> --> */}
-                <div>
+                <div data-aos="zoom-in">
                     <div className="paddingrow rowmargin">
                         <h1 className="HeadOne">Latest News</h1>
                     </div>
